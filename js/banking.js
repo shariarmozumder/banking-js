d0c79239bc9e017622413969b02275e920a7c659
shariarmozumder <mdshariar00@gmail.com>
2022-05-16 20:31:12 +0600

    second commit

---
+++ js/banking.js
@@ -0,0 +1,55 @@
+// Handel Deposit Button Event
+
+document.getElementById("deposit-button").addEventListener("click", function(){
+    //get the amount deposited
+    const depositInput=document.getElementById("deposit-input");
+
+    const newDepositAmountText =depositInput.value;
+
+    //update deposit total
+    const newDepositAmount=parseFloat(newDepositAmountText);
+
+    const depositTotal=document.getElementById("deposit-total");
+
+    const previousDepositText= depositTotal.innerText;
+    const previousDepositAmount =parseFloat(previousDepositText);
+    const newDepositTotal= previousDepositAmount + newDepositAmount;
+
+    depositTotal.innerText=newDepositTotal;
+
+    //update account balance
+    const balanceTotal = document.getElementById("balance-total");
+    const balanceTotalText=balanceTotal.innerText;
+    const previousBalanceTotal=parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText=newBalanceTotal;
+
+    //clear the deposit Input
+    depositInput.value="";
+})
+
+//Handelwithdraw event handler
+
+document.getElementById("withdraw-button").addEventListener("click", function(){
+   const withdrawInput=document.getElementById("withdraw-input");
+   const withdrawAmountText=withdrawInput.value;
+   const newWithdrawAmount =parseFloat(withdrawAmountText);
+   console.log(newWithdrawAmount);
+
+   //set withdraw Total
+   const withdrawTotal=document.getElementById("withdraw-total");
+   const previousWithdrawText=withdrawTotal.innerText;
+   const previousWithdrawTotal =parseFloat(previousWithdrawText);
+   const newWithdrawTotal=previousWithdrawTotal + newWithdrawAmount;
+   withdrawTotal.innerText= newWithdrawTotal;
+
+   //Update Balance
+   const balanceTotal= document.getElementById("balance-total");
+   const previousBalanceText=balanceTotal.innerText;
+   const previousBalanceTotal= parseFloat(previousBalanceText);
+   const newBalanceTotal= previousBalanceTotal - newWithdrawAmount;
+   balanceTotal.innerText=newBalanceTotal;
+
+   //clear withdraw input
+   withdrawInput.value= "";
+})
---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.getElementById("login-submit").addEventListener("click", function (){
+    const emailField=document.getElementById("user-email");
+    const userEmail=emailField.value;
+    //get password
+    const passwordField=document.getElementById("user-password");
+    const userPassword= passwordField.value;
+    if(userEmail == "shariarbai@gmail.com" && userPassword == "boss"){
+        window.location.href="banking.html"
+    }
+})
+
+
+
